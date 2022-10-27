261360eace692d4d35f2cae21425e60e65fa8261
Luke Oguro <lukeoguro@gmail.com>
2022-10-27 13:07:51 +0900

    Add new note form and capability to filter by importance

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import Note from './components/Note'
+import { useState } from 'react';
+import Note from './components/Note';
 
-function App({ notes }) {
+function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [content, setContent] = useState('A new note...');
+  const [showAll, setShowAll] = useState(true);
+  const notesToShow = showAll ? notes : notes.filter(({ important: i }) => i);
+
+  function handleContentChange(e) {
+    setContent(e.target.value);
+  }
+
+  function handleNoteSubmit(e) {
+    e.preventDefault();
+
+    const newNote = {
+      content,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(newNote));
+    setContent('');
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={handleNoteSubmit}>
+        <input value={content} onChange={handleContentChange} />
+        <button type="submit">Save</button>
+      </form>
     </div>
   )
 }
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,5 @@
 function Note({ note }) {
-  return (
-    <li>{note.content}</li>
-  )
+  return note.important ? <li><b>{note.content}</b></li> : <li>{note.content}</li>;
 }
 
 export default Note;
